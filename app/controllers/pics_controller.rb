f801bed8d5a63f052fb9f0917d171c4a11d43be7
senhad <senadhadrovic@gmail.com>
2016-09-05 11:02:52 +0800

    added acts_as_votable gem and set up

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,6 @@
 class PicsController < ApplicationController
-before_action :find_pic, only: [:show, :edit, :update, :destroy]
-
+before_action :find_pic, only: [:show, :edit, :update, :destroy, :upvote]
+before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@pics = Pic.all.order("created_at DESC")
@@ -38,6 +38,11 @@ before_action :find_pic, only: [:show, :edit, :update, :destroy]
 	def destroy
 		@pic.destroy
 		redirect_to root_path
+	end 
+
+	def upvote
+		@pic.upvote_by current_user
+		redirect_to :back
 	end 
 
 	private
